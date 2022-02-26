c2f17e2afbc5a5f5fff2134cb67326fdc13686a9
Altynbek Kasymov <Altynbek.Kasymov.sss@gmail.com>
2022-02-26 19:12:43 +0600

    Task 10 done

--- Task9/app.js
+++ Task9/app.js
@@ -1,39 +1,73 @@
-function Auto(model, year, color, volume) {
-  this.model = model;
-  this.year = year;
-  this.color = color;
-  this.volume = volume;
+class Restaurant {
+  constructor(name, cuisineType) {
+    this.name = name;
+    this.cuisineType = cuisineType;
+    this.served = 0;
+  }
+  showInfo() {
+    return `Название ресторана: ${this.name}. Тип кухни: ${this.cuisineType}`;
+  }
+  status() {
+    const time = new Date().getHours();
+    if (time >= 9 && time <= 22) return " Ресторан открыт ";
+    else return " Ресторан закрыт ";
+  }
+
+  serve() {
+    return (this.served = this.served + 1);
+  }
+
+  setServed(amount) {
+    return (this.served = this.served + amount);
+  }
+
+  showTotalServed() {
+    return `Обслужено: ${this.served}`;
+  }
 }
 
-Auto.prototype.showInfo = function () {
-  return `${this.model}, ${this.year}, ${this.color}, ${this.volume}`;
-};
+const badobot = new Restaurant("Badobot", "Kyrgyz ");
 
-Auto.prototype.drive = function () {
-  if (this.volume > 0) return (this.volume -= 10);
-  else {
-    return "Volume empty";
+badobot.serve();
+badobot.setServed(15);
+document.write(
+  badobot.showInfo() +
+    "<br>" +
+    badobot.status() +
+    "<br>" +
+    badobot.showTotalServed() +
+    "<br>" +
+    "<br>"
+);
+
+class Auto {
+  constructor(model, year, power, color) {
+    this.model = model;
+    this.year = year;
+    this.power = power;
+    this.color = color;
   }
-};
+  showInfo() {
+    return `Модель авто: ${this.model}, год выпуска: ${this.year}, мощность двигателя: ${this.power}, цвет кузова: ${this.color}`;
+  }
+}
 
-const tesla = new Auto("Tesla Model X", 2020, "black", 150);
-const mers = new Auto("Mersedes S", 2020, "darkblue", 80);
+class EVAuto extends Auto {
+  constructor(model, year, power, color, battery) {
+    super(model, year, power, color);
+    this.battery = battery;
+  }
+  showInfo() {
+    return super.showInfo() + ", ёмкость батареи: " + this.battery;
+  }
 
-console.log(tesla);
-console.log(mers);
+  promote() {
+    return `Покупайте электромобили. Используйте солнце а не горючее.`;
+  }
+}
 
-console.log(tesla.showInfo());
-console.log(tesla.drive());
-console.log(tesla.showInfo());
-console.log(tesla.drive());
-console.log(tesla.showInfo());
-console.log(mers.drive());
-console.log(mers.drive());
-console.log(mers.drive());
-console.log(mers.drive());
-console.log(mers.drive());
-console.log(mers.drive());
-console.log(mers.drive());
-console.log(mers.drive());
+const mers = new Auto("Mers 600", 2020, "160hp", "black");
+document.write(mers.showInfo() + "<br>");
 
-console.log(mers.showInfo());
+const tesla = new EVAuto("Model X", 2021, "180hp", "gray", "140kv");
+document.write(tesla.showInfo() + "<br>" + tesla.promote());
